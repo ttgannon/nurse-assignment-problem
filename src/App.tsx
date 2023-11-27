eabecb985f54f53ddd10f3a686437260bf413c3a
ttgannon <ga1179@nyu.edu>
2023-11-27 09:46:08 -0500

    replacing accessToken verification

--- src/App.tsx
+++ src/App.tsx
@@ -42,33 +42,36 @@ const App = () => {
     <Container className="p-3">
       <Container className="p-5 mb-4 bg-light rounded-3">
         <h1>Nursify</h1>
-        {/* {accessToken ? 
-        ( */}
-        <Card>
-          <Card.Header>Unit Selection</Card.Header>
-          <Card.Body>
-            <UnitSelection
-              units={units}
-              onChange={(id) => {
-                const unitId = units.find((unit) => unit.id.toString() === id);
+        {accessToken ? (
+          <Card>
+            <Card.Header>Unit Selection</Card.Header>
+            <Card.Body>
+              <UnitSelection
+                units={units}
+                onChange={(id) => {
+                  const unitId = units.find(
+                    (unit) => unit.id.toString() === id,
+                  );
 
-                if (!unitId) return;
+                  if (!unitId) return;
 
-                const selectedUnit: Unit = {
-                  ...unitId,
-                  nurses: nurses.filter((nurse) => nurse.unitId === unitId?.id),
-                  patients: patients.filter(
-                    (patient) => patient.unitId === unitId?.id,
-                  ),
-                };
-                setSelectedUnit(selectedUnit);
-              }}
-            />
-          </Card.Body>
-        </Card>
-        {/* ) : (
+                  const selectedUnit: Unit = {
+                    ...unitId,
+                    nurses: nurses.filter(
+                      (nurse) => nurse.unitId === unitId?.id,
+                    ),
+                    patients: patients.filter(
+                      (patient) => patient.unitId === unitId?.id,
+                    ),
+                  };
+                  setSelectedUnit(selectedUnit);
+                }}
+              />
+            </Card.Body>
+          </Card>
+        ) : (
           <Auth setAccessToken={handleAccessToken} />
-        )} */}
+        )}
 
         {selectedUnit && (
           <>
