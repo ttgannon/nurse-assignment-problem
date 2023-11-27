47967ca3e48ade4fc400de538420bdddb9486bbc
ttgannon <ga1179@nyu.edu>
2023-11-27 12:27:00 -0500

    added new styling assets and a styles file

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,9 @@ import {
   Auth,
 } from "./components";
 import { Nurse, Unit } from "./interfaces";
-import { Card } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 import { useDummyData } from "./hooks";
+import "./assets/styles/homepage.css";
 
 const App = () => {
   const { units, nurses: nurseData, patients } = useDummyData();
@@ -39,83 +40,114 @@ const App = () => {
   }
 
   return (
-    <Container className="p-3">
-      <Container className="p-5 mb-4 bg-light rounded-3">
-        <h1>Nursify</h1>
-        {accessToken ? (
-          <Card>
-            <Card.Header>Unit Selection</Card.Header>
+    <>
+      {accessToken ? (
+        <Container className="p-3">
+          <Container className="p-5 mb-4 bg-light rounded-3">
+            <Card>
+              <Card.Header>Unit Selection</Card.Header>
+              <Card.Body>
+                <UnitSelection
+                  units={units}
+                  onChange={(id) => {
+                    const unitId = units.find(
+                      (unit) => unit.id.toString() === id,
+                    );
+
+                    if (!unitId) return;
+
+                    const selectedUnit: Unit = {
+                      ...unitId,
+                      nurses: nurses.filter(
+                        (nurse) => nurse.unitId === unitId?.id,
+                      ),
+                      patients: patients.filter(
+                        (patient) => patient.unitId === unitId?.id,
+                      ),
+                    };
+                    setSelectedUnit(selectedUnit);
+                  }}
+                />
+              </Card.Body>
+            </Card>
+          </Container>
+        </Container>
+      ) : (
+        <Container className="homePage">
+          <Row>
+            <Col>
+              <h1 className="nursifyLogo">Nursify</h1>
+            </Col>
+            <Row>
+              <img
+                src="src/assets/nurses.jpg"
+                style={{ maxWidth: 900 + "px", margin: 0 }}
+              ></img>
+              <Col className="align-items-center justify-content-center">
+                <h1>Welcome to Nursify</h1>
+                <h3>Making safer, fairer assignments.</h3>
+                <Auth setAccessToken={handleAccessToken} />
+              </Col>
+            </Row>
+          </Row>
+        </Container>
+      )}
+
+      {selectedUnit && (
+        <>
+          <Card className="mt-4">
+            <Card.Header>Nurses</Card.Header>
             <Card.Body>
-              <UnitSelection
+              <Alert variant="primary" dismissible>
+                These are the nurses we have on {selectedUnit?.name} for the
+                shift. Remove nurses who aren't coming in, and add new ones who
+                aren't already scheduled.
+              </Alert>
+              <NurseTable
+                nurses={selectedUnit.nurses}
                 units={units}
-                onChange={(id) => {
-                  const unitId = units.find(
-                    (unit) => unit.id.toString() === id,
+                removeNurse={(employeeId) => {
+                  setNurses((nurses) =>
+                    nurses.filter((nurse) => nurse.employeeId !== employeeId),
                   );
-
-                  if (!unitId) return;
-
-                  const selectedUnit: Unit = {
-                    ...unitId,
-                    nurses: nurses.filter(
-                      (nurse) => nurse.unitId === unitId?.id,
-                    ),
-                    patients: patients.filter(
-                      (patient) => patient.unitId === unitId?.id,
-                    ),
-                  };
-                  setSelectedUnit(selectedUnit);
+                }}
+                addNurse={(nurse: Nurse) => {
+                  setNurses(() => [...nurses, nurse]);
                 }}
               />
             </Card.Body>
           </Card>
-        ) : (
-          <Auth setAccessToken={handleAccessToken} />
-        )}
-
-        {selectedUnit && (
-          <>
-            <Card className="mt-4">
-              <Card.Header>Nurses</Card.Header>
-              <Card.Body>
-                <Alert variant="primary" dismissible>
-                  These are the nurses we have on {selectedUnit?.name} for the
-                  shift. Remove nurses who aren't coming in, and add new ones
-                  who aren't already scheduled.
-                </Alert>
-                <NurseTable
-                  nurses={selectedUnit.nurses}
-                  units={units}
-                  removeNurse={(employeeId) => {
-                    setNurses((nurses) =>
-                      nurses.filter((nurse) => nurse.employeeId !== employeeId),
-                    );
-                  }}
-                  addNurse={(nurse: Nurse) => {
-                    setNurses(() => [...nurses, nurse]);
-                  }}
-                />
-              </Card.Body>
-            </Card>
-            <Card className="mt-4">
-              <Card.Header>Patients</Card.Header>
-              <Card.Body>
-                <PatientTable patients={selectedUnit.patients} units={units} />
-              </Card.Body>
-            </Card>
-            <Card className="mt-4">
-              <Card.Header>Assignment</Card.Header>
-              <Card.Body>
-                <Assignment
-                  nurses={selectedUnit.nurses}
-                  patients={selectedUnit.patients}
-                />
-              </Card.Body>
-            </Card>
-          </>
-        )}
+          <Card className="mt-4">
+            <Card.Header>Patients</Card.Header>
+            <Card.Body>
+              <PatientTable patients={selectedUnit.patients} units={units} />
+            </Card.Body>
+          </Card>
+          <Card className="mt-4">
+            <Card.Header>Assignment</Card.Header>
+            <Card.Body>
+              <Assignment
+                nurses={selectedUnit.nurses}
+                patients={selectedUnit.patients}
+              />
+            </Card.Body>
+          </Card>
+        </>
+      )}
+      <Container className="loginContainer">
+        <Row>
+          <Col>
+            <h5>Pricing</h5>
+          </Col>
+          <Col>
+            <h5>Terms</h5>
+          </Col>
+          <Col>
+            <h5>Contact</h5>
+          </Col>
+        </Row>
       </Container>
-    </Container>
+    </>
   );
 };
 
