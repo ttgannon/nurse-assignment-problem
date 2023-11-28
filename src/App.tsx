bb1694d0ee98d4cfb25b6d502039e79f9c90106a
ttgannon <ga1179@nyu.edu>
2023-11-27 23:11:39 -0500

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,7 @@ const App = () => {
       {accessToken ? (
         <Container className="p-3">
           <Container className="p-5 mb-4 bg-light rounded-3">
+            <h1>Select your unit tonight</h1>
             <Card>
               <Card.Header>Unit Selection</Card.Header>
               <Card.Body>
@@ -195,43 +196,62 @@ const App = () => {
 
       {selectedUnit && (
         <>
-          <Card className="mt-4">
-            <Card.Header>Nurses</Card.Header>
-            <Card.Body>
-              <Alert variant="primary" dismissible>
-                These are the nurses we have on {selectedUnit?.name} for the
-                shift. Remove nurses who aren't coming in, and add new ones who
-                aren't already scheduled.
-              </Alert>
-              <NurseTable
-                nurses={selectedUnit.nurses}
-                units={units}
-                removeNurse={(employeeId) => {
-                  setNurses((nurses) =>
-                    nurses.filter((nurse) => nurse.employeeId !== employeeId),
-                  );
-                }}
-                addNurse={(nurse: Nurse) => {
-                  setNurses(() => [...nurses, nurse]);
-                }}
-              />
-            </Card.Body>
-          </Card>
-          <Card className="mt-4">
-            <Card.Header>Patients</Card.Header>
-            <Card.Body>
-              <PatientTable patients={selectedUnit.patients} units={units} />
-            </Card.Body>
-          </Card>
-          <Card className="mt-4">
-            <Card.Header>Assignment</Card.Header>
-            <Card.Body>
-              <Assignment
-                nurses={selectedUnit.nurses}
-                patients={selectedUnit.patients}
-              />
-            </Card.Body>
-          </Card>
+          <Container className="p-3">
+            <Container className="p-5 mb-4 bg-light rounded-3">
+              <h1>Incoming nurses</h1>
+              <Card className="mt-4">
+                <Card.Header>Nurses</Card.Header>
+                <Card.Body>
+                  <Alert variant="primary" dismissible>
+                    These are the nurses we have on {selectedUnit?.name} for the
+                    shift. Remove nurses who aren't coming in, and add new ones
+                    who aren't already scheduled.
+                  </Alert>
+                  <NurseTable
+                    nurses={selectedUnit.nurses}
+                    units={units}
+                    removeNurse={(employeeId) => {
+                      setNurses((nurses) =>
+                        nurses.filter(
+                          (nurse) => nurse.employeeId !== employeeId,
+                        ),
+                      );
+                    }}
+                    addNurse={(nurse: Nurse) => {
+                      setNurses(() => [...nurses, nurse]);
+                    }}
+                  />
+                </Card.Body>
+              </Card>
+            </Container>
+          </Container>
+          <Container className="p-3">
+            <Container className="p-5 mb-4 bg-light rounded-3">
+              <h1>Patients on the floor</h1>
+              <Card className="mt-4">
+                <Card.Header>Patients</Card.Header>
+                <Card.Body>
+                  <PatientTable
+                    patients={selectedUnit.patients}
+                    units={units}
+                  />
+                </Card.Body>
+              </Card>
+            </Container>
+          </Container>
+          <Container className="p-3">
+            <Container className="p-5 mb-4 bg-light rounded-3">
+              <Card className="mt-4">
+                <Card.Header>Assignment</Card.Header>
+                <Card.Body>
+                  <Assignment
+                    nurses={selectedUnit.nurses}
+                    patients={selectedUnit.patients}
+                  />
+                </Card.Body>
+              </Card>
+            </Container>
+          </Container>
         </>
       )}
       <Container fluid className="loginContainer">
