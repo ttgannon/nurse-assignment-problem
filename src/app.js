257a5de39830af3629e9eb784e6f2592dcd42af9
ttgannon <ga1179@nyu.edu>
2023-12-27 13:29:00 -0300

    adding basic express routing and error handling

--- src/app.js
+++ src/app.js
@@ -2,6 +2,27 @@ import express from 'express';
 
 export const app = express()
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+/** 404 handler */
+
+app.use(function(req, res, next) {
+    const err = new Error("Not Found", 404);
+    return next(err);
+  });
+  
+/** general error handler */
+  
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    return res.json({
+        error: err,
+        message: err.message
+    });
+});
