2660432c1c1cc0c50fd7df7b1098880f435c19c1
ttgannon <ga1179@nyu.edu>
2023-12-28 16:04:01 -0300

    start server for backend routing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 /* server.js starts the server. Run using node server.js. */
 
-import { app } from "./app";
+import { app } from "./app.js";
 
 app.listen(3000, function () {
   console.log("Listening on 3000");
