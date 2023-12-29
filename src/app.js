b7f424145395adac73595ad03b3036c398aafcc9
ttgannon <ga1179@nyu.edu>
2023-12-29 13:57:10 -0300

    moving sequelize instance to app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 import express from 'express';
-import { sequelize } from '../models/index.js';
+import { sequelize } from './database/index.js';
+import { NurseModel, PatientModel, UnitModel } from "../models/index.js";
 
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+/* Sequelize */
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+  sequelize.sync()
+  .then(async () => {
+    console.log('Database synchronized');
+  })
+  .catch((error) => {
+    console.error('Error synchronizing database:', error);
+});
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+  throw new Error(
+    "Oops! There was a database error. Try connecting to the database again.",
+  );
+}
+
+UnitModel.hasMany(PatientModel, {
+  foreignKey: 'unit'
+});
+
+UnitModel.hasMany(NurseModel, {
+  foreignKey: 'unit'
+});
 
 
   /* Routes */
