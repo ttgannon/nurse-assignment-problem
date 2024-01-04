cf98943d9951c2be46e0229c3d5bbfe19d57f38f
ttgannon <ga1179@nyu.edu>
2024-01-04 14:41:28 -0300

    made patient list scrollable

--- src/components/DemoComponents/DemoPatientTable.tsx
+++ src/components/DemoComponents/DemoPatientTable.tsx
@@ -9,25 +9,29 @@ export const DemoPatientTable = ({
   units: Unit | null;
 }) => {
   return (
-    <Table striped bordered>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Unit</th>
-        </tr>
-      </thead>
-      <tbody>
-        {patients.map(({ id, first_name, last_name, patient_id, unit }) => (
-          <tr key={id}>
-            <td>{patient_id}</td>
-            <td>{first_name}</td>
-            <td>{last_name}</td>
-            <td>{unit || "N/A"}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <>
+      <div style={{ height: "400px", overflowY: "auto" }}>
+        <Table striped bordered responsive>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Unit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {patients.map(({ id, first_name, last_name, patient_id, unit }) => (
+              <tr key={id}>
+                <td>{patient_id}</td>
+                <td>{first_name}</td>
+                <td>{last_name}</td>
+                <td>{unit || "N/A"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </>
   );
 };
