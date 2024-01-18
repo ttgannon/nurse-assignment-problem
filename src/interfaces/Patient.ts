aa89d8f747c4a62f4e689672f94d136ea73f68b4
ttgannon <ga1179@nyu.edu>
2024-01-18 20:25:17 -0300

    adding data to patient interface to match data from db

--- src/interfaces/Patient.ts
+++ src/interfaces/Patient.ts
@@ -4,4 +4,14 @@ export interface Patient {
   last_name: string;
   first_name: string;
   unit: string;
+  acuityScore: number;
+  continent: boolean;
+  independently_mobile: boolean;
+  num_meds: number;
+  high_risk_meds: number;
+  num_devices: number;
+  num_drips: number;
+  new_trach: number;
+  tube_feeds: number;
+  wound_care: number;
 }
